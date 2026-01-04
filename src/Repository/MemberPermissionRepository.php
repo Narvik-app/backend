48354f4d65139d758698d5371ddc8c916e677f86
Benoît VIGNAL <git@benoit-vignal.fr>
2026-01-04 17:27:58 +0100

    feat: Permission management

--- src/Repository/MemberPermissionRepository.php
+++ src/Repository/MemberPermissionRepository.php
@@ -2,9 +2,8 @@
 
 namespace App\Repository;
 
+use App\Entity\ClubDependent\Member;
 use App\Entity\ClubDependent\MemberPermission;
-use App\Entity\UserMember;
-use App\Enum\Permission;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -17,29 +16,10 @@ class MemberPermissionRepository extends ServiceEntityRepository {
   }
 
   /**
-   * Find all permissions for a given UserMember
+   * Find all permissions for a given Member
    * @return MemberPermission[]
    */
-  public function findByUserMember(UserMember $userMember): array {
-    return $this->findBy(['userMember' => $userMember]);
-  }
-
-  /**
-   * Check if a UserMember has a specific permission
-   */
-  public function hasPermission(UserMember $userMember, Permission $permission): bool {
-    return $this->findOneBy([
-      'userMember' => $userMember,
-      'permission' => $permission,
-    ]) !== null;
-  }
-
-  /**
-   * Get all permission values for a UserMember
-   * @return Permission[]
-   */
-  public function getPermissionValues(UserMember $userMember): array {
-    $permissions = $this->findByUserMember($userMember);
-    return array_map(fn(MemberPermission $mp) => $mp->getPermission(), $permissions);
+  public function findByMember(Member $member): array {
+    return $this->findBy(['member' => $member]);
   }
 }
