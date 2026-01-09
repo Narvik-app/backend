e2d2e3f74954e4a3788a6b7adacad55ad5f38b71
Benoît VIGNAL <git@benoit-vignal.fr>
2026-01-09 17:29:53 +0100

    style: code cleanup

--- src/Repository/ClubDependent/PermissionTemplateRepository.php
+++ src/Repository/ClubDependent/PermissionTemplateRepository.php
@@ -3,7 +3,7 @@
 namespace App\Repository\ClubDependent;
 
 use App\Entity\ClubDependent\PermissionTemplate;
-use App\Entity\Club;
+use App\Repository\Trait\ClubLinkedTrait;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -11,15 +11,9 @@ use Doctrine\Persistence\ManagerRegistry;
  * @extends ServiceEntityRepository<PermissionTemplate>
  */
 class PermissionTemplateRepository extends ServiceEntityRepository {
+  use ClubLinkedTrait;
+
   public function __construct(ManagerRegistry $registry) {
     parent::__construct($registry, PermissionTemplate::class);
-  }
-
-  /**
-   * Find all templates for a given club
-   * @return PermissionTemplate[]
-   */
-  public function findByClub(Club $club): array {
-    return $this->findBy(['club' => $club], ['name' => 'ASC']);
   }
 }
