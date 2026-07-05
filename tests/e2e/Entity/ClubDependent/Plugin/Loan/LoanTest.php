a7e36b84eeeb9d462eb0b44468ae7668f8bf0de2
Benoît VIGNAL <git@benoit-vignal.fr>
2026-07-05 22:40:06 +0200

    Code quality

--- tests/e2e/Entity/ClubDependent/Plugin/Loan/LoanTest.php
+++ tests/e2e/Entity/ClubDependent/Plugin/Loan/LoanTest.php
@@ -129,7 +129,7 @@ class LoanTest extends AbstractEntityClubLinkedTestCase {
     $loan = LoanFactory::createOne(['loanItem' => $item, 'endDate' => null]);
 
     $this->loggedAsAdminClub1();
-    $this->makePatchRequest($this->getIriFromResource($loan), ['endDate' => (new \DateTimeImmutable())->format('c')]);
+    $this->makePatchRequest($this->getIriFromResource($loan), ['endDate' => new \DateTimeImmutable()->format('c')]);
     $this->assertResponseIsSuccessful();
   }
 
