5d59bcbde5f1e86dfc471ac7672e3fd542e67d1e
Benoît VIGNAL <git@benoit-vignal.fr>
2026-09-16 19:08:48 +0200

    Simplify

--- src/Entity/ClubDependent/ClubSetting.php
+++ src/Entity/ClubDependent/ClubSetting.php
@@ -65,6 +65,8 @@ use Symfony\Component\Validator\Constraints as Assert;
     'groups' => ['club-setting', 'club-setting-write'],
   ], order: ['name' => 'asc'],)]
 class ClubSetting extends UuidEntity implements ClubLinkedEntityInterface {
+  public const string DEFAULT_SMIC_HOURLY_RATE = '12.31';
+
   public static function getClubSqlPath(): string {
     return "club";
   }
@@ -103,9 +105,6 @@ class ClubSetting extends UuidEntity implements ClubLinkedEntityInterface {
 
   #[Groups(['common-read', 'club-setting-read'])]
   private ?Season $currentSeason = null;
-
-  /** Current legal French SMIC hourly rate — update here whenever the regulation changes. */
-  public const string DEFAULT_SMIC_HOURLY_RATE = '12.31';
 
   /** SMIC hourly rate used to value declared time in the time-and-travel plugin */
   #[ORM\Column(type: \Doctrine\DBAL\Types\Types::DECIMAL, precision: 6, scale: 2, nullable: true)]
